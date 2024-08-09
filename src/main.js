bb89c686768d0d6765dabc04e22a744a6e9d985e
Guilherme Schumann <gui.rsc45@gmail.com>
2024-08-08 22:07:55 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import '../styles/mordern-normalize.css';
+import '../styles/style.css';
+import '../styles/hero.css';
+import '../styles/processo.css';
+import '../styles/servicos.css';
+import '../styles/footer.css';
